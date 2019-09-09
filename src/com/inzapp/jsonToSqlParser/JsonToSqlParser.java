3783324a8685aedf5ad817148cb135b4494d6bd3
inzapp <keg9903@naver.com>
2019-09-09 13:30:59 +0900

    DeParser 클래스 생성, saveFile메소드 구현

--- src/com/inzapp/jsonToSqlParser/JsonToSqlParser.java
+++ src/com/inzapp/jsonToSqlParser/JsonToSqlParser.java
@@ -1,12 +1,14 @@
 package com.inzapp.jsonToSqlParser;
 
 import com.inzapp.jsonToSqlParser.config.Config;
+import com.inzapp.jsonToSqlParser.core.DeParser;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 
-public class JsonToSqlParser {
+public class JsonToSqlParser extends DeParser {
     public static void main(String[] args) {
         String inputFileName = Config.INPUT_FILE_NAME;
         String outputFileName = Config.OUTPUT_FILE_NAME;
@@ -15,10 +17,23 @@ public class JsonToSqlParser {
             outputFileName = args[1];
         }
 
-        JSONObject json = readJsonFromFile(inputFileName);
+        JsonToSqlParser jsonToSqlParser = new JsonToSqlParser();
+        JSONObject json = jsonToSqlParser.readJsonFromFile(inputFileName);
+        String sql = jsonToSqlParser.deParse(json);
+
+        try {
+            System.out.println("input json\n\n");
+            System.out.println(json.toString(4));
+            System.out.println("output sql\n\n");
+            System.out.println(sql);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        jsonToSqlParser.saveFile(sql, outputFileName);
     }
 
-    private static JSONObject readJsonFromFile(String fileName) {
+    private JSONObject readJsonFromFile(String fileName) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             StringBuilder sb = new StringBuilder();
@@ -35,6 +50,15 @@ public class JsonToSqlParser {
             return json;
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    private void saveFile(String sql, String fileName) {
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            fos.write(sql.getBytes());
+        }catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/com/inzapp/jsonToSqlParser/core/DeParser.java
@@ -0,0 +1,9 @@
+package com.inzapp.jsonToSqlParser.core;
+
+import org.json.JSONObject;
+
+public class DeParser {
+    public String deParse(JSONObject json) {
+        return "sql";
+    }
+}
