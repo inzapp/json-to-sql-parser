f4b8dba6a462914d0153c8337e513a6ed266371c
inzapp <keg9903@naver.com>
2019-09-09 13:13:32 +0900

    config, json key 추가

---
+++ src/com/inzapp/jsonToSqlParser/config/Config.java
@@ -0,0 +1,7 @@
+package com.inzapp.jsonToSqlParser.config;
+
+public class Config {
+    public static final String INPUT_FILE_NAME = "input.json";
+    public static final String OUTPUT_FILE_NAME = "output.txt";
+    public static final String SQL_SYNTAX_ERROR = "sql syntax error";
+}
---
+++ src/com/inzapp/jsonToSqlParser/config/JsonKey.java
@@ -0,0 +1,23 @@
+package com.inzapp.jsonToSqlParser.config;
+
+public class JsonKey {
+    public static final String CRUD = "CRUD";
+    public static final String INSERT = "INSERT";
+    public static final String SELECT = "SELECT";
+    public static final String UPDATE = "UPDATE";
+    public static final String DELETE = "DELETE";
+    public static final String DISTINCT = "DISTINCT";
+    public static final String COLUMN = "COLUMN";
+    public static final String FROM = "TABLE";
+    public static final String FROM_ALIAS = "TABLE ALIAS";
+    public static final String FROM_SUB_QUERY = "TABLE SUB QUERY ";
+    public static final String FROM_SUB_QUERY_ANALYSE = "TABLE SUB QUERY ANALYSE ";
+    public static final String WHERE = "WHERE";
+    public static final String WHERE_SUB_QUERY = "WHERE SUB QUERY ";
+    public static final String WHERE_SUB_QUERY_ANALYSE = "WHERE SUB QUERY ANALYSE ";
+    public static final String VALUE = "VALUE";
+    public static final String GROUP_BY = "GROUP_BY";
+    public static final String ORDER_BY = "ORDER_BY";
+    public static final String JOIN = "JOIN ";
+    public static final String JOIN_ALIAS = "JOIN ALIAS ";
+}
